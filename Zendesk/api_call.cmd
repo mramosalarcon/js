1b728fe25473a631727adc888b7d7a9bd574ffb0
Miguel Ramos Alarcón <miguel.vazquez@orioninc.com>
2024-12-04 05:14:34 -0600

    Test

--- Zendesk/api_call.cmd
+++
@@ -1,4 +0,0 @@
-curl --request GET \
-  --url 'https://casd.zendesk.com/api/v2/tickets.json?page=9' \
-  --header 'Authorization: Basic YWRtaW5tYWNAY2FzZC5jb20ubXgvdG9rZW46R3dnVzI2bVBhVXA2VnpXYWdnZkJyQ0hvbEdzZ1F2eUpZN1F5T2M3Nw' \
-  --header 'Content-Type: application/json' \
